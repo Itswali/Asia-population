f9c8bda0528a1974e83228a773d29b0d19a8a40e
Wali Muhammad <luckywali0986@gmail.com>
2023-02-08 14:50:45 +0500

    approaching different detail for routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes, Link } from 'react-router-dom';
-import Detail from './components/Detail';
 import Home from './components/Home';
+
+const DetailPage = ({ match }) => <div>Detail Page for {match.params.id}</div>;
+
 
 function App() {
   return (
@@ -9,13 +11,13 @@ function App() {
       <ul>
         <li>
           <Link to="/"><img src="https://www.shutterstock.com/image-illustration/aesthetic-illustration-girl-sitting-on-260nw-1919058461.jpg" alt="logo" /></Link>
-          <Link to="Details"> <img src="https://www.shutterstock.com/image-illustration/aesthetic-anime-theme-illustration-boy-260nw-1762336295.jpg" alt="Logo2" /> </Link>
+          <Link to="detail/1"> <img src="https://www.shutterstock.com/image-illustration/aesthetic-anime-theme-illustration-boy-260nw-1762336295.jpg" alt="Logo2" /> </Link>
         </li>
       </ul>
     </nav>
     <Routes>
 <Route path="/" element={<Home />} />
-<Route path="Details/" element={<Detail />} />
+<Route path="detail/:id" component={<DetailPage />} />
 
   </Routes>
     </>
