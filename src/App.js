d62fa4e26e6fc075dba4e61fa89c615cd802cffa
Wali Muhammad <luckywali0986@gmail.com>
2023-02-08 14:46:09 +0500

    add Routes and Link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import sample from "./components/sample";
+import { Route, Routes, Link } from 'react-router-dom';
+import Detail from './components/Detail';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
-    
+    <nav>
+      <ul>
+        <li>
+          <Link to="/"><img src="https://www.shutterstock.com/image-illustration/aesthetic-illustration-girl-sitting-on-260nw-1919058461.jpg" alt="logo" /></Link>
+          <Link to="Details"> <img src="https://www.shutterstock.com/image-illustration/aesthetic-anime-theme-illustration-boy-260nw-1762336295.jpg" alt="Logo2" /> </Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+<Route path="/" element={<Home />} />
+<Route path="Details/" element={<Detail />} />
+
+  </Routes>
     </>
   );
 }
---
+++ src/components/Detail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Detail() {
+  return (
+    <div>
+      Detail Page.
+      
+    </div>
+  )
+}
