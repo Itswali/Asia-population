fb9be041b72bcf2413465cc752ca0f5fe30a6abf
Wali Muhammad <luckywali0986@gmail.com>
2023-02-09 20:33:56 +0500

    link redux store in react through provider

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import './style/style.css'
+import store from './redux/ConfigureStore';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
+
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
